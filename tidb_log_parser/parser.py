bf17ddc33bd7f68a79b649b51a30f0379bcf30ad
fabregaszy <yanzhang1989@icloud.com>
2018-07-17 22:22:40 +0800

    sort log by cost time

--- tidb_log_parser/parser.py
+++ tidb_log_parser/parser.py
@@ -10,19 +10,22 @@ class TiDBLogParser(object):
 
     @classmethod
     def parse(cls, log_file):
+        slow_query_logs = []
         with open(log_file) as f:
-            slow_query_logs = []
             for line in f:
                 if 'SLOW_QUERY' in line:
                     try:
                         slow_query_log = SlowQueryLog(line)
-                        # debug:
-                        print(slow_query_log)
                         slow_query_logs.append(slow_query_log)
                     except InvalidTiDBLogError as e:
                         print(e)
                         continue
             print('total slow query count: %d' % len(slow_query_logs))
+        # sort by time
+        slow_query_logs.sort(key=lambda x: x.cost_time, reverse=True)
+        print('top 3 slow query:')
+        for l in slow_query_logs[:3]:
+            print(l)
 
 
 if __name__ == '__main__':
